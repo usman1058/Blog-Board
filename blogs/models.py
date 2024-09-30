17f5491d82b477732e25bda306111a8fe9461ee7
usman1058 <techinfo1058@gmail.com>
2024-09-30 08:53:10 -0700

    2

--- blogs/models.py
+++ blogs/models.py
@@ -1,8 +1,14 @@
 
 from django.db import models
+# from core import settings      # another way to call  settings.py
+from  django.conf import settings
+# from django.contrib.auth.models import User
 # from django.urls import reverse
 
 class Blog(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # auther = models.ForeignKey(User)  # ANOTHER WAY TO ADD A FOREGIN KEY PARAMETER
+    # auther = models.ForeignKey("auth.user")  # ANOTHER WAY TO ADD A FOREGIN KEY PARAMETER
     title = models.CharField(max_length=200)
     content = models.TextField()  # Ensure this is defined
     
