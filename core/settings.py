a78cf297e14c61713739088b0fb852d04b192f4a
usman1058 <techinfo1058@gmail.com>
2024-10-02 06:50:56 -0700

    2

--- core/settings.py
+++ core/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'blogs/static'),  # Ensure this matches your actual path
+    os.path.join(BASE_DIR,'static'),  # Ensure this matches your actual path
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
